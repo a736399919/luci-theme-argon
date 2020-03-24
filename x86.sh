f1fc3c74b9c4f92b85b8aea4fe8026e86aaf55d1
a736399919 <55206982+a736399919@users.noreply.github.com>
2020-03-24 21:11:16 +0800

    Create x86.sh

---
+++ x86.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#=================================================
+#   Description: DIY script
+#   Lisence: MIT
+#   Author: P3TERX
+#   Blog: https://p3terx.com
+#=================================================
+#克隆源码
+git clone -b dev-19.07 https://github.com/Lienol/openwrt
+cd openwrt
+sed -i '$a src-git extra https://github.com/Andy2244/openwrt-extra.git' feeds.conf.default
+./scripts/feeds clean
+./scripts/feeds update -a
+./scripts/feeds install -f -p extra -a
+./scripts/feeds install -a
+
+#修改lan口地址
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+#修改机器名称
+sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
+
+#修改wifi名称
+sed -i 's/OpenWrt/FK20100010/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+#修改时区
+#sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+#修改banner
+rm -rf package/base-files/files/etc/banner
+cp -f ../banner package/base-files/files/etc/
+[ -e ../G-DOCK/default.config ] && mv -f ../G-DOCK/default.config .config
+[ -e ../G-DOCK/Lienol-19.07*.config ] && mv -f ../G-DOCK/Lienol-19.07*.config .config
